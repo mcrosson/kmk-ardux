80eed62cde46bc4e1e1c45a12bdf62232a15f79c
KemoNine <kemonin@kemonine.info>
2023-07-30 10:26:26 -0400

    rework settings.toml to read easier ; add kb2040 option to the settings ; put kb2040 neopixel definition behind a conditional based on the setttings

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ else:
 
 #####
 # NeoPixel on kb2040 (tune accordingly / remove if different mcu)
-from kmk.extensions.RGB import RGB, AnimationModes
-rgb_ext = RGB(
-    pixel_pin=board.NEOPIXEL,
-    num_pixels=1,
-    val_limit=100,
-    val_default=25,
-    animation_mode=AnimationModes.BREATHING_RAINBOW
-)
-ardux_keyboard.extensions.append(rgb_ext)
+if os.getenv('ARDUX_RGB_KB2040'):
+    from kmk.extensions.RGB import RGB, AnimationModes
+    rgb_ext = RGB(
+        pixel_pin=board.NEOPIXEL,
+        num_pixels=1,
+        val_limit=100,
+        val_default=25,
+        animation_mode=AnimationModes.BREATHING_RAINBOW
+    )
+    ardux_keyboard.extensions.append(rgb_ext)
 
 #####
 # Main
