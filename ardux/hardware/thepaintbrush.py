5d524cb33a8996365c6664d8ee521ae6877f3086
KemoNine <kemonine@kemonine.info>
2025-03-21 17:06:32 -0400

    add support for nice nano (includes combo timeout adjustments) and disable the ble name in settings.toml by default

--- ardux/hardware/thepaintbrush.py
+++ ardux/hardware/thepaintbrush.py
@@ -7,6 +7,8 @@ if 'kb2040' == mcu:
     from kmk.quickpin.pro_micro.kb2040 import pinout as pins
 elif 'sparkfun_promicro_rp2040' == mcu:
     from kmk.quickpin.pro_micro.sparkfun_promicro_rp2040 import pinout as pins
+elif 'nice_nano' == mcu:
+    from kmk.quickpin.pro_micro.nice_nano import pinout as pins
 else:
     print('Unsupported mcu: ', os.getenv('ARDUX_MCU'))
     raise NotImplementedError('Unsupported mcu:'+ os.getenv('ARDUX_MCU'))
@@ -18,7 +20,6 @@ class ThePaintbrushArduxKeyboard(_ArduxKeyboardStandard):
         # Direct wire & matrix setup
         self.matrix = KeysScanner(
             [pins[16], pins[17], pins[18], pins[19], 
-             pins[12], pins[13], pins[14], pins[15]],
-             value_when_pressed=False
+                pins[12], pins[13], pins[14], pins[15]],
+            value_when_pressed=False
         )
-
--- boot.py
+++ boot.py
@@ -28,6 +28,8 @@ if 'kb2040' == mcu:
     from kmk.quickpin.pro_micro.kb2040 import pinout as pins
 elif 'sparkfun_promicro_rp2040' == mcu:
     from kmk.quickpin.pro_micro.sparkfun_promicro_rp2040 import pinout as pins
+elif 'nice_nano' == mcu:
+    from kmk.quickpin.pro_micro.nice_nano import pinout as pins
 else:
     print('Unsupported mcu: ', os.getenv('ARDUX_MCU'))
     raise NotImplementedError('Unsupported mcu:'+ os.getenv('ARDUX_MCU'))
